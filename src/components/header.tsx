0ab85cf26f5eae0882bbe6b6d49edc23749c1546
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-07-20 15:56:23 +0900

    🚧 in progress

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,26 +1,30 @@
 import { NextPage } from "next";
 import React from "react";
+// masthead{display:table;width:100%;height:auto;
+// background: linear - gradient(90deg, rgba(255, 255, 255, .1) 0, rgba(255, 255, 255, .1) 100 %), url(../img/bg - meguro1.jpg); background - position:center center; background - repeat: no - repeat; background - size: cover}
+
 
 export const HeaderBlock: NextPage = () => {
   return (
     <>
-      <div className="masthead d-flex" id="page-top">
+      <div
+        className="min-h-[50rem] relative h-auto w-full py-[8rem] bg-center bg-cover flex justify-around"
+        style={{
+          backgroundImage: "url('/img/bg-meguro1.jpg')",
+        }}
+        id="page-top"
+      >
         <div className="container text-center my-auto">
-          <h1
-            className="mb-5"
-            style={{ color: "rgb(241, 238, 238)", fontSize: "60px" }}
-          >
-            音の世界へ
-          </h1>
-          <h3
-            className="mb-5"
-            style={{ color: "rgb(241, 238, 238)", fontSize: "20px" }}
-          >
+          <h1 className="mb-10 text-6xl text-white">音の世界へ</h1>
+          <h3 className="mb-10 text-xl text-white">
             あなたの音楽旅行は 櫻井ピアノスクール とともにここから始まります。
             <br />
             貴方の目的地はどこですか？
           </h3>
-          <a className="btn btn-primary btn-xl js-scroll-trigger" href="#intro">
+          <a
+            className="bg-[#126a87] text-white text=lg px-10 py-6 rounded  js-scroll-trigger"
+            href="#intro"
+          >
             楽しさと、美しさを分かち合う
           </a>
         </div>
