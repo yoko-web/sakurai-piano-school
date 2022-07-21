c06f4364749c07201548f1ced48facc612dbcd5a
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-07-21 19:54:07 +0900

    fix error in console

--- src/components/FAQ/faq.tsx
+++ src/components/FAQ/faq.tsx
@@ -2,14 +2,6 @@ import Faq from "react-faq-component";
 import { Heading2 } from "../atoms";
 import { data } from "./data";
 
-const styles = {
-  bgColor: "",
-  titleTextColor: "#126a87",
-  rowTitleColor: "gray",
-  rowContentColor: "grey",
-  arrowColor: "#126a87",
-};
-
 const config = {
   animate: true,
   arrowIcon: "A",
@@ -18,10 +10,10 @@ const config = {
 
 export const FAQ = () => {
   return (
-    <div className="py-28 text-center px-2 sm:px-6 mx-auto w-screen" id="about">
+    <div className="py-28 text-center px-2 sm:px-6 mx-auto w-screen" id="faq">
       <Heading2 type="gray"> よくあるご質問</Heading2>
       <div className="max-w-2/3 text-left">
-        <Faq data={data} styles={styles} config={config} />
+        <Faq data={data} config={config} />
       </div>
     </div>
   );
