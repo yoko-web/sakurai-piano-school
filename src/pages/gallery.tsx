06aaecdcf0dac3c5b23e41e5aec7ae345d0616e2
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-07-24 00:57:52 +0900

    head title set

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,12 +1,13 @@
+import Head from "next/head";
 import { Layout } from "../components/layout";
-import {
-  SwiperCards,
-  SwiperCardsDesktop,
-} from "../components/molecules/swiper";
+import { SwiperCardsDesktop } from "../components/molecules/swiper";
 
 const Gallery = () => {
   return (
     <div className="">
+      <Head>
+        <title>Gallery - 座間市のピアノ教室 櫻井ピアノスクール</title>
+      </Head>
       <Layout>
         <div className="bg-primary w-screen p-3 text-white pb-20 text-lg">
           Gallery
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import { Layout } from "../components/layout";
 import { ProfileMain } from "../components/organisms/profile";
 
 const Gallery = () => {
   return (
     <Layout>
+      <Head>
+        <title>講師プロフィール - 座間市のピアノ教室 櫻井ピアノスクール</title>
+      </Head>
       <div className=" bg-primary">
         <div
           className=" bg-cover bg-top max-w-7xl mx-auto "
