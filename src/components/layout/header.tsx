2ee43c7a5048109668422871894cd4cc1a30c8bf
yoko-at-home <tw107sf.ham@icloud.com>
2023-09-17 22:46:16 +0900

    文言追加

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -10,9 +10,12 @@ export const HeaderBlock: NextPage = () => {
       }}
       id="page-top"
     >
-      <div className="absolute max-h-screen min-w-screen bg-white/10 top-[20%] p-20">
-        <div className="container text-center my-auto">
-          <h1 className="mb-16 text-5xl  md:text-7xl text-white font-black shadow-sm sm:bg-none whitespace-nowrap">
+      <div className="absolute max-h-screen min-w-screen bg-black/20 top-[20%] p-20">
+        <div className="mb-16 text-xl md:text-2xl text-primary font-bold font-mono text-right">
+          ピアノ科が教える本格ピアノ教室
+        </div>
+        <div className="container text-left my-auto">
+          <h1 className="my-16 text-5xl  md:text-7xl text-white font-black shadow-sm sm:bg-none whitespace-nowrap">
             音の世界へ
           </h1>
           <h3 className="mb-10 text-xl md:text-2xl text-white font-bold">
@@ -20,7 +23,7 @@ export const HeaderBlock: NextPage = () => {
             <br />
             貴方の目的地はどこですか？
           </h3>
-          <div className="py-10">
+          <div className="py-10 text-center">
             <NavButton href="intro">楽しさと、美しさを分かち合う</NavButton>
           </div>
         </div>
