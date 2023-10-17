d220ff90986904e4ea910402d0bd28cd8f03ed39
yoko-at-home <tw107sf.ham@icloud.com>
2023-10-17 15:07:44 +0900

    update navigation design

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -11,10 +11,10 @@ export const HeaderBlock: NextPage = () => {
       id="page-top"
     >
       <div className="absolute h-screen w-screen bg-black/20 top-0 px-10 py-20 sm:p-20">
-        <span className="text-xl md:text-2xl text-primary font-mono  p-1 sm:bg-white rounded-r text-right">
-          ピアノ科が教える 本格ピアノ教室
+        <span className="text-xl md:text-2xl text-white font-mono  p-1 rounded-r text-right">
+          ピアノ科が教える ピアノ教室
         </span>
-        <div className="flex flex-col justify-around text-left h-3/4">
+        <div className="mt-5 flex flex-col justify-around text-left h-3/4">
           <h1 className="text-5xl md:text-7xl text-white font-black shadow-sm sm:bg-none whitespace-nowrap">
             音の世界へ
           </h1>
